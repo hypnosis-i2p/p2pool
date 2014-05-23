fd4d2241cf4d4fd427bacf751f90c7141621ba98
CartmanSPC <CartmanSPC@server.fake>
2014-05-23 14:32:20 -0700

    Update SUBSIDY_FUNC for potcoin

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -351,7 +351,7 @@ nets = dict(
             'potcoinaddress' in (yield bitcoind.rpc_help()) and
             not (yield bitcoind.rpc_getinfo())['testnet']
         )),
-        SUBSIDY_FUNC=lambda height: 420*100000000 >> (height + 1)//840000,
+        SUBSIDY_FUNC=lambda height: 420*100000000 >> (height + 1)//280000,
         POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data)),
         BLOCK_PERIOD=40, # s
         SYMBOL='POT',
