da937f7a6ec3fe000ce427207f724b520a92fe10
CartmanSPC <CartmanSPC@server.fake>
2014-07-21 14:22:25 -0700

    SUBSIDY_FUNC change for Casinocoin
    Takes affect at block 575000.

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -263,7 +263,7 @@ nets = dict(
             'casinocoinaddress' in (yield bitcoind.rpc_help()) and
             not (yield bitcoind.rpc_getinfo())['testnet']
         )),
-        SUBSIDY_FUNC=lambda height: 50*100000000 >> (height + 1)//3153600,
+        SUBSIDY_FUNC=lambda height: 10*100000000,
         POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data)),
         BLOCK_PERIOD=30, # s targetspacing
         SYMBOL='CSC',
