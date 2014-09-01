6079f026e22ffcfb80a5ac1937005ab2b9d5ccd0
K1773R <K1773R@darkgamex.ch>
2014-09-01 16:15:28 +0200

    setting max new tx to 500000

--- p2pool/data.py
+++ p2pool/data.py
@@ -140,7 +140,7 @@ class Share(object):
             else:
                 if known_txs is not None:
                     this_size = bitcoin_data.tx_type.packed_size(known_txs[tx_hash])
-                    if new_transaction_size + this_size > 50000: # only allow 50 kB of new txns/share
+                    if new_transaction_size + this_size > 500000: # only allow 500 kB of new txns/share
                         break
                     new_transaction_size += this_size
                 new_transaction_hashes.append(tx_hash)
@@ -360,7 +360,7 @@ class Share(object):
                 return True, 'txs over block size limit'
             
             new_txs_size = sum(bitcoin_data.tx_type.packed_size(known_txs[tx_hash]) for tx_hash in self.share_info['new_transaction_hashes'])
-            if new_txs_size > 50000:
+            if new_txs_size > 500000:
                 return True, 'new txs over limit'
         
         return False, None
