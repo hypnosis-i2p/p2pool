527e027b04233c0cde4f82fec42e49bcaf98a8d2
K1773R <K1773R@darkgamex.ch>
2014-09-01 16:15:46 +0200

    change anoncoin settings, due to hardfork

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -290,9 +290,9 @@ nets = dict(
         BLOCK_PERIOD=205, # s targetspacing
         SYMBOL='ANC',
         CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'anoncoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/anoncoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.anoncoin'), 'anoncoin.conf'),
-        BLOCK_EXPLORER_URL_PREFIX='https://coinplorer.com/ANC/Blocks/',
-        ADDRESS_EXPLORER_URL_PREFIX='https://coinplorer.com/ANC/Addresses/',
-        TX_EXPLORER_URL_PREFIX='https://coinplorer.com/ANC/Transactions/',
+        BLOCK_EXPLORER_URL_PREFIX = 'http://darkgamex.ch:2751/block/',
+        ADDRESS_EXPLORER_URL_PREFIX = 'http://darkgamex.ch:2751/address/',
+        TX_EXPLORER_URL_PREFIX = 'http://darkgamex.ch:2751/tx/',
         SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
         DUMB_SCRYPT_DIFF=2**16,
         DUST_THRESHOLD=0.03e8,
